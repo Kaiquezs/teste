a72f0866f45364d2a4d313c6a925340f91a9545e
Kaiquezs <kaiquez.s.santana@gmail.com>
2024-11-09 12:54:00 -0300

    Add files via upload

---
+++ confirmar.js
@@ -0,0 +1,24 @@
+const buttonAlert = document.getElementById("btn-confirmar");
+    if (buttonAlert) {
+        buttonAlert.onclick = function() {
+            alert("Seu pedido foi Confirmado!");
+        };
+    }
+
+    var texto = document.getElementById("nome");
+    var textoPedido = document.getElementById("pedido");
+    var textoTotal = document.getElementById("total");
+    var nome = localStorage.getItem('nome');
+    const pedidos = JSON.parse(localStorage.getItem('pedido'))
+    var total = JSON.parse(localStorage.getItem("total"));
+    var array = [];
+
+    textoPedido.innerHTML = "<strong>Pedido: </strong>"+pedidos;
+    textoTotal.innerHTML = "<strong>Total: </strong>"+total;
+    texto.innerHTML = "<strong>Nome</strong>: "+nome;
+
+    window.addEventListener("beforeunload", function (event) {
+        this.localStorage.removeItem("pedido");
+        this.localStorage.removeItem("total");
+    });
+    
