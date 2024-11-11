7d532bd7de554988f69fef30fbcc8426cfc932ce
Kaiquezs <kaiquez.s.santana@gmail.com>
2024-11-10 22:27:53 -0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,3 +1,35 @@
+var salada = document.getElementById("saladaVerde");
+var batataFrita = document.getElementById("batataFrita");
+var petiscos = document.getElementById("petiscos");
+var bolinhoBacalhau = document.getElementById("bolinhoBacalhau");
+var feijoada = document.getElementById("feijoada");
+var fileParmegiana = document.getElementById("fileParmegiana");
+var moquecaPeixe = document.getElementById("moquecaPeixe");
+var frangoAssadoComErvas = document.getElementById("frangoAssadoComErvas");
+var lasanhaBolonhesa = document.getElementById("lasanhaBolonhesa");
+var macarronada = document.getElementById("macarronada");
+var escondidinhoCarneSeca = document.getElementById("escondidinhoCarneSeca");
+var brigadeiro = document.getElementById("brigadeiro");
+var petitGateau = document.getElementById("petitGateau");
+var pudimLeite = document.getElementById("pudimLeite");
+var mousseMaracuja = document.getElementById("mousseMaracuja");
+var tortaMorango = document.getElementById("tortaMorango");
+var boloChocolate = document.getElementById("boloChocolate");
+var sucoLimao = document.getElementById("sucoLimao");
+var sucoLaranja = document.getElementById("sucoLaranja");
+var sucoMaracuja = document.getElementById("sucoMaracuja");
+var sucoGoiaba = document.getElementById("sucoGoiaba");
+var sucoManga = document.getElementById("sucoManga");
+var cerveja = document.getElementById("cerveja");
+var whisky = document.getElementById("whisky");
+var agua = document.getElementById("agua");
+var cafeExpresso = document.getElementById("cafeExpresso");
+var valor = 0;
+
+var confirmar = document.getElementById("confirmar");
+var pedido = [];
+var info = [];
+
 function verificarCheckboxes() {
     var checkboxes = document.querySelectorAll('input[name="produto"]:checked');
     if (checkboxes.length > 0) {
@@ -8,11 +40,13 @@ function verificarCheckboxes() {
 
     function salvarInformacoes() {
 if (verificarCheckboxes()) {
-alert('Acertou')
-
+    calcularValor();
+    localStorage.setItem('pedido', JSON.stringify(pedido));
+    localStorage.setItem('total', JSON.stringify(this.valor));
+    window.location.href = "pagConfirmar.html";
 
 } else {
-alert('Por favor, selecione pelo menos um produto.');
+    alert('Por favor, selecione pelo menos um produto.');
 }}
 
 
@@ -55,41 +89,7 @@ function getInfo(){
     return localStorage.getItem('nome');
 }
 
-var salada = document.getElementById("saladaVerde");
-var batataFrita = document.getElementById("batataFrita");
-var petiscos = document.getElementById("petiscos");
-var bolinhoBacalhau = document.getElementById("bolinhoBacalhau");
-var feijoada = document.getElementById("feijoada");
-var fileParmegiana = document.getElementById("fileParmegiana");
-var moquecaPeixe = document.getElementById("moquecaPeixe");
-var frangoAssadoComErvas = document.getElementById("frangoAssadoComErvas");
-var lasanhaBolonhesa = document.getElementById("lasanhaBolonhesa");
-var macarronada = document.getElementById("macarronada");
-var escondidinhoCarneSeca = document.getElementById("escondidinhoCarneSeca");
-var brigadeiro = document.getElementById("brigadeiro");
-var petitGateau = document.getElementById("petitGateau");
-var pudimLeite = document.getElementById("pudimLeite");
-var mousseMaracuja = document.getElementById("mousseMaracuja");
-var tortaMorango = document.getElementById("tortaMorango");
-var boloChocolate = document.getElementById("boloChocolate");
-var sucoLimao = document.getElementById("sucoLimao");
-var sucoLaranja = document.getElementById("sucoLaranja");
-var sucoMaracuja = document.getElementById("sucoMaracuja");
-var sucoGoiaba = document.getElementById("sucoGoiaba");
-var sucoManga = document.getElementById("sucoManga");
-var cerveja = document.getElementById("cerveja");
-var whisky = document.getElementById("whisky");
-var agua = document.getElementById("agua");
-var cafeExpresso = document.getElementById("cafeExpresso");
-var valor = 0;
-
-var confirmar = document.getElementById("confirmar");
-var pedido = [];
-var info = [];
-
-localStorage.removeItem("pedido");
-
-confirmar.onclick = function(valor){
+function calcularValor(valor){
     this.valor = 0;
     var verificar = localStorage.getItem("pedido");
     if(verificar !== null){
@@ -100,161 +100,159 @@ confirmar.onclick = function(valor){
     if(salada.checked){
         valor = 12;
         this.valor = valor;
-        info = "salada";
-        pedido.push("salada");
+        info = "Salada";
+        pedido.push(info);
     }
     if(batataFrita.checked){
         valor = 10;
         this.valor += valor;
-        info = "batata frita";
-        pedido.push("batata frita");
+        info = "Batata frita";
+        pedido.push(info);
     }
     if(petiscos.checked){
         valor = 52;
         this.valor += valor;
-        info = "petiscos";
-        pedido.push("petiscos");
+        info = "Petiscos";
+        pedido.push(info);
     }
     if(bolinhoBacalhau.checked){
         valor = 12;
         this.valor += valor;
-        info = "bolinho de bacalhau";
-        pedido.push("bolinho de bacalhau");
+        info = "Bolinho de bacalhau";
+        pedido.push(info);
     }
     if(feijoada.checked){
         valor = 35;
         this.valor += valor;
         info = "Feijoada";
-        pedido.push("Feijoada");
+        pedido.push(info);
     }
     if(fileParmegiana.checked){
         valor = 40;
         this.valor += valor;
         info = "Filé á parmegiana";
-        pedido.push("Filé á parmegiana");
+        pedido.push(info);
     }
     if(moquecaPeixe.checked){
         valor = 45;
         this.valor += valor;
         info = "Moqueca de peixe";
-        pedido.push("Moqueca de peixe");
+        pedido.push(info);
     }
     if(frangoAssadoComErvas.checked){
         valor = 30;
         this.valor += valor;
         info = "Frango assado com ervas";
-        pedido.push("Frango assado com ervas");
+        pedido.push(info);
     }
     if(lasanhaBolonhesa.checked){
         valor = 35;
         this.valor += valor;
         info = "Lasanha bolonhesa";
-        pedido.push("Lasanha bolonhesa");
+        pedido.push(info);
     }
     if(macarronada.checked){
         valor = 28;
         this.valor += valor;
         info = "Macarronada";
-        pedido.push("Macarronada");
+        pedido.push(info);
     }
     if(escondidinhoCarneSeca.checked){
         valor = 28;
         this.valor += valor;
         info = "Escondidinho de carne seca";
-        pedido.push("Escodidinho de carne seca");
+        pedido.push(info);
     }
     if(brigadeiro.checked){
         valor = 6;
         this.valor += valor;
         info = "Brigadeiro";
-        pedido.push("Brigadeiro");
+        pedido.push(info);
     }
     if(petitGateau.checked){
         valor = 18;
         this.valor += valor;
         info = "Petit Gateau";
-        pedido.push("Petit Gateau");
+        pedido.push(info);
     }
     if(pudimLeite.checked){
         valor = 12;
         this.valor += valor;
         info = "Pudim de leite";
-        pedido.push("Pudim de leite");
+        pedido.push(info);
     }
     if(mousseMaracuja.checked){
         valor = 10;
         this.valor += valor;
         info = "Mousse de maracujá";
-        pedido.push("Mousse de maracujá");
+        pedido.push(info);
     }
     if(tortaMorango.checked){
         valor = 15;
         this.valor += valor;
         info = "Torta de morango";
-        pedido.push("Torta de morango");
+        pedido.push(info);
     }
     if(boloChocolate.checked){
         valor = 10;
         this.valor += valor;
         info = "Bolo de chocolate";
-        pedido.push("Bolo de chocolate");
+        pedido.push(info);
     }
     if(sucoLimao.checked){
         valor = 5;
         this.valor += valor;
         info = "Suco de limão";
-        pedido.push("Suco de limão");
+        pedido.push(info);
     }
     if(sucoLaranja.checked){
         valor = 5;
         this.valor += valor;
         info = "Suco de laranja";
-        pedido.push("Suco de laranja");
+        pedido.push(info);
     }
     if(sucoMaracuja.checked){
         valor = 5;
         this.valor += valor;
         info = "Suco de maracujá";
-        pedido.push("Suco de maracujá");
+        pedido.push(info);
     }
     if(sucoGoiaba.checked){
         valor = 5;
         this.valor += valor;
         info = "Suco de goiaba";
-        pedido.push("Suco de goiaba");
+        pedido.push(info);
     }
     if(sucoManga.checked){
         valor = 5;
         this.valor += valor;
         info = "Suco de manga";
-        pedido.push("Suco de manga");
+        pedido.push(info);
     }
     if(cerveja.checked){
         valor = 10;
         this.valor += valor;
         info = "Cerveja";
-        pedido.push("Cerveja");
+        pedido.push(info);
     }
     if(whisky.checked){
         valor = 20;
         this.valor += valor;
         info = "Whisky";
-        pedido.push("Whisky");
+        pedido.push(info);
     }
     if(agua.checked){
         valor = 3;
         this.valor += valor;
         info = "Água";
-        pedido.push("Água");
+        pedido.push(info);
     }
     if(cafeExpresso.checked){
         valor = 3;
         this.valor += valor;
         info = "Café expresso";
-        pedido.push("Café expresso");
+        pedido.push(info);
     }
-    localStorage.setItem('pedido', JSON.stringify(pedido));
-    localStorage.setItem('total', JSON.stringify(this.valor));
 }
 
 window.addEventListener("beforeunload", function (event) {
